f1f0630ddc82ce4b911bbf673cceb6af6bad828e
ColluraV <valerio.18.02.89@hotmail.it>
2023-07-11 17:11:45 +0200

    completo e funzionante

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ const fieldContainer = document.querySelector("[id=field-container]");
 
 //selezione difficoltà
 btnConferma.addEventListener("click",function(){
+   fieldContainer.innerHTML="";
+   
     let totblocchi = 0;
 
     const lvlSelect = document.querySelector("#difficolta").value;
@@ -20,29 +22,41 @@ btnConferma.addEventListener("click",function(){
         if(lvlSelect==="difficile"){
             totblocchi= 49 }
 
-    console.log(totblocchi)
+    console.log(totblocchi);
 
-    const campoPieno = fieldGen (totblocchi)
+    const campoPieno = fieldGen (totblocchi);
+
+    for (let i = 0; i<campoPieno.length; i++){
+        fieldContainer.append(campoPieno[i]);
+    }
 
 
-    fieldContainer.append(campoPieno)
-    console.log(campoPieno)
+    console.log(campoPieno);
 })
 
 
 
 
   //generazione singolo blocco
-function blockGen (contentBlock, totBlocchi){
+
+/**
+ * 
+ * @param {*} totBlocchi 
+ * @param {number} numBlocchi 
+ * @returns 
+ */
+function blockGen (contenuto, numBlocchi){
     const div = document.createElement("div");
-    const blocchiPerRiga = Math.sqrt(totBlocchi);
-    
-    div.classList.add("quadrato")
+    const blocchiPerRiga = Math.sqrt(numBlocchi);
+    div.classList.add("quadrato");
+    div.style.flexBasis =`calc( 100% / ${blocchiPerRiga})`
+    console.log(blocchiPerRiga);
 
     //inserimento testo interno al div da sostituire con ciò che più aggrada
 
-    div.textContent=contentBlock;
+    div.innerHTML= contenuto;
 
+    return div;
 }
 
 
@@ -53,10 +67,11 @@ function fieldGen (numBlocchi) {
   
     for (let i = 0; i < numBlocchi; i++) {
       
-      const blocco = blockGen("sqr" + i, numBlocchi);
+      const blocco = blockGen("blocco" + (i+1) , numBlocchi);
   
       campo.push(blocco);
-    } console.log(campo)
+
+    }
     return campo;
   }
 
